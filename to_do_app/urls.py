6f98c0ba04d8180f13193a7ca68c915b512ad6a0
ansul710 <ansul71098@gmail.com>
2020-02-24 17:31:42 +0530

    commit

--- to_do_app/urls.py
+++ to_do_app/urls.py
@@ -1,6 +1,8 @@
-
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    #path('', ),
+    path('to-do/', todoView, name='todohome'),
+    path('to_do_add/', additems, name='to_do_add_new'),
+    path('to_do_delete/<int:post_id>/', deleteitem, name='delete_item'),
 ]
--- to_do_app/views.py
+++ to_do_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import TodoItems
+
 
 # Create your views here.
+
+
+def todoView(request):
+    items = TodoItems.objects.all()
+    content = {'todos': items}
+    return render(request, 'todo.html', content)
+
+
+def additems(request):
+    new_item = TodoItems(content=request.POST['content'])
+    new_item.save()
+    return redirect('todohome')
+
+
+def deleteitem(request, post_id):
+    delete_item = TodoItems.objects.get(id=post_id)
+    delete_item.delete()
+    return redirect('todohome')
